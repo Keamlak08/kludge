06489c1e2f16eb65fce1c7a47c64cea3435e6aaa
Keamlak Kebede <keamlakk@gmail.com>
2026-03-22 13:09:18 -0400

    set background image; created assets for images to come

---
+++ src/config/assets.js
@@ -0,0 +1,14 @@
+// KULDGE - asset paths
+
+// cobwebs to add to the vibe
+export const COBWEBS = {
+    heroTopLeft: null,
+    heroTopRight: null,
+}
+
+// background
+export const BG = '/pipes.png'
+
+// this is going to be a cartoon handyman fixing
+// the pipes in a looping gif, but I don't have it yet
+export const HANDYMAN = null
